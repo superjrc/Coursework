861d4db188e6a122f50469b69f326429ac090bba
superjrc <jasper@tridonkai.com>
2022-11-21 16:37:27 +0000

    -fixed collision -added debug text -added tileset -added tunnel blueprint(vertical only) -added 200x200 room blueprint

--- main.py
+++ main.py
@@ -1,104 +1,127 @@
 import pygame
-import sys
 import random
-import numpy
-import time
 
 # setup variables
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((810, 600))
 screen.fill("black")
 clock = pygame.time.Clock()
 walls = []
 font = pygame.font.SysFont("Comic Sans MS", 25)
+tileset = []
+
+for x in range(0, 12):
+    for y in range(0, 16):
+        tileset.insert(x, [x * 50, y * 50])
+tileset.sort()
+print(tileset)
+
 
 # classes
 class Player:
-    def __init__(self, x=10, y=10, hp=100, xspeed=0, yspeed=0):
-        self.rectcoll = None
+    def __init__(self):
+        self.rect_coll = None
         self.icon = pygame.image.load("player.png")
         self.rect = self.icon.get_rect()
-        self.x = x
-        self.y = y
-        self.xspeed = xspeed
-        self.yspeed = yspeed
-        self.hp = hp
+        self.x = 10
+        self.y = 10
+        self.x_speed = 0
+        self.y_speed = 0
+        self.hp = 100
 
     def checkMove(self):
         key_press = pygame.key.get_pressed()
         if key_press[pygame.K_LEFT]:
-            self.xspeed = -2
+            self.x_speed = -2
         if key_press[pygame.K_RIGHT]:
-            self.xspeed = 2
+            self.x_speed = 2
         if key_press[pygame.K_UP]:
-            self.yspeed = 2
+            self.y_speed = 2
         if key_press[pygame.K_DOWN]:
-            self.yspeed = -2
-
-
+            self.y_speed = -2
 
         self.rect.midleft = (p.x, p.y)
 
     def playerMove(self):
-        if self.yspeed > 1:
+        if self.y_speed > 1:
             self.y = self.y - 2
-        if self.yspeed < 0:
+        if self.y_speed < 0:
             self.y = self.y + 2
-        if self.xspeed < 0:
+        if self.x_speed < 0:
             self.x = self.x - 2
-        if self.xspeed > 1:
+        if self.x_speed > 1:
             self.x = self.x + 2
-        self.yspeed = 0
-        self.xspeed = 0
+        self.y_speed = 0
+        self.x_speed = 0
 
-    def CheckCollision(self,rectcoll):
-        if self.rect.colliderect(rectcoll):
-            print(rectcoll.top - 16)
-            self.xspeed = self.xspeed * 0
-            self.yspeed = self.yspeed * 0
-            if self.x >= (rectcoll.left - 30) and self.x < (rectcoll.right - 30):
-                self.x = self.x - 2
-            elif self.x <= (rectcoll.right) and self.x > rectcoll.left:
-                self.x = self.x + 2
-            if self.y <= (rectcoll.top - 14):
-                self.y = self.y - 5
+    def Stop(self):
+        self.x_speed = 0
+        self.y_speed = 0
 
+    def CheckCollision(self, rect_coll):
+        if self.rect.colliderect(rect_coll):
+            self.Stop()
+            if (rect_coll.left - 30) <= self.x < (rect_coll.right - 30):
+                self.x_speed = -2
+            if rect_coll.right >= self.x > rect_coll.left:
+                self.x_speed = 2
+            if self.y <= (rect_coll.top - 14):
+                self.y_speed = 2
+            if self.y >= (rect_coll.bottom - 14):
+                self.y_speed = -2
+        if p.x <= 0:
+            self.Stop()
+            self.x_speed = 2
+        if p.x >= 770:
+            self.Stop()
+            self.x_speed = -2
+        if p.y <= 15:
+            self.Stop()
+            self.y_speed = -2
+        if p.y >= 585:
+            self.Stop()
+            self.y_speed = 2
 
 
 class Dungeon:
     def __init__(self, length=0, width=0):
+        self.offsetx = None
+        self.offsety = None
         self.wall = pygame.image.load("wall.png")
         self.floor = pygame.image.load("floor.png")
         self.length = length
         self.width = width
         self.tiles = []
 
+    def MakeTunnel(self, n, hor):
+        self.width = 10
+        self.length = 50
+        self.offsetx = tileset[n][0]
+        self.offsety = tileset[n][1]
+        for i in range(2):
+            walls.append([self.offsety + 50 * i * hor, self.offsetx, self.width, self.length])
 
+    def MakeBox200(self, n):
+        self.width = 10
+        self.length = 50
+        self.offsetx = tileset[n][0]
+        self.offsety = tileset[n][1]
+        for i in range(2):
+            walls.append([self.offsety + 200 * i, self.offsetx, self.width, 200])
+            walls.append([self.offsety, self.offsetx + 200 * i, 210, self.width])
 
-    def MakeWall(self):
-        self.width = random.randint(100, 200)
-        self.length = random.randint(100, 200)
-        self.offsetx = random.randint(100, 200)
-        self.offsety = random.randint(100, 200)
-        walls.append([self.offsety, self.offsetx, self.width, self.length])
-
-
-
-    def Draw1(self):
-
-        for i in walls:
-            z = pygame.draw.rect(screen, (255, 255, 255), (i[2], i[3], i[0], i[1]))
-            p.CheckCollision(z)
-
+def Draw():
+    for i in walls:
+        z = pygame.draw.rect(screen, (255, 255, 255), (i[0], i[1], i[2], i[3]))
+        p.CheckCollision(z)
 
 
 d = Dungeon()
 p = Player()
 Player()
 
-d.MakeWall()
-d.MakeWall()
-# d.GenRoom()
+d.MakeTunnel(4,1)
+d.MakeBox200(12)
 
 # main game loop
 while True:
@@ -107,14 +130,14 @@ while True:
             quit()
     screen.fill("black")
     p.checkMove()
-    d.Draw1()
+    Draw()
     p.playerMove()
-    text = font.render((str(p.y)), False, (255, 255, 0))
-    screen.blit(text, (0,0))
-
-
-    # d.GenRoom()
-    # screen.blit(walls, (0,10))
+    text = font.render((str(p.x)), False, (255, 255, 0))
+    text1 = font.render((str(p.y)), False, (255, 255, 0))
+    text2 = font.render((str(int(clock.get_fps()))), False, (255, 255, 0))
+    screen.blit(text, (0, 0))
+    screen.blit(text1, (50, 0))
+    screen.blit(text2, (100, 0))
     screen.blit(p.icon, p.rect)
     pygame.display.flip()
     clock.tick(60)
