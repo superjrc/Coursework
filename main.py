6d63046afebe0172bce90a9a10bbadfc361b5a64
jibbs <jasper@tridonkai.com>
2022-11-21 11:09:10 +0000

    REctange

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import random
 import numpy
+import time
 
 # setup variables
 pygame.init()
@@ -10,28 +11,53 @@ screen.fill("black")
 clock = pygame.time.Clock()
 walls = []
 
+
 # classes
 class Player:
-    def __init__(self, x=10, y=10, hp=100):
+    def __init__(self, x=10, y=10, hp=100, xspeed=0, yspeed=0):
+        self.rectcoll = None
         self.icon = pygame.image.load("player.png")
         self.rect = self.icon.get_rect()
         self.x = x
         self.y = y
+        self.xspeed = xspeed
+        self.yspeed = yspeed
         self.hp = hp
 
     def checkMove(self):
         key_press = pygame.key.get_pressed()
         if key_press[pygame.K_LEFT]:
-            self.x = self.x - 2
+            self.xspeed = -2
         if key_press[pygame.K_RIGHT]:
-            self.x = self.x + 2
+            self.xspeed = 2
         if key_press[pygame.K_UP]:
-            self.y = self.y - 2
+            self.yspeed = 2
         if key_press[pygame.K_DOWN]:
-            self.y = self.y + 2
+            self.yspeed = -2
+
+
 
         self.rect.midleft = (p.x, p.y)
 
+    def playerMove(self):
+        if self.yspeed > 1:
+            self.y = self.y - 2
+        if self.yspeed < 0:
+            self.y = self.y + 2
+        if self.xspeed < 0:
+            self.x = self.x - 2
+        if self.xspeed > 1:
+            self.x = self.x + 2
+        self.yspeed = 0
+        self.xspeed = 0
+
+    def CheckCollision(self,rectcoll):
+        if self.rect.colliderect(rectcoll):
+            self.xspeed = self.xspeed * -1
+
+            self.yspeed = self.yspeed * -1
+            self.x = self.x + self.xspeed
+            self.y = self.y + self.yspeed
 
 class Dungeon:
     def __init__(self, length=0, width=0):
@@ -41,40 +67,22 @@ class Dungeon:
         self.width = width
         self.tiles = []
 
-    def MakeWallImg(self):
-        self.width = random.randint(200, 400)
-        self.length = random.randint(200, 400)
-        for x in range(0, self.width):
-            if self.width > -45:
-                self.tiles.append(self.width)
-                self.width = self.width - 45
+
 
     def MakeWall(self):
-        self.width = random.randint(100,200)
-        self.length = random.randint(100,200)
-        self.offsetx = random.randint(100,200)
-        self.offsety = random.randint(100,200)
-        walls.append(pygame.Rect((self.offsety, self.offsetx),(self.width, self.length)))
+        self.width = random.randint(100, 200)
+        self.length = random.randint(100, 200)
+        self.offsetx = random.randint(100, 200)
+        self.offsety = random.randint(100, 200)
+        walls.append([self.offsety, self.offsetx, self.width, self.length])
 
 
-
-    def ShowWall(self, walltype, offsetx, offsety):
-        if walltype == 0:
-            for y in self.tiles:
-                screen.blit(self.wall, (offsetx, y + offsety))
-        elif walltype == 1:
-            for y in self.tiles:
-                screen.blit(self.wall, (y + offsetx, offsety))
-
-    def GenRoom(self):
-        for x in range(random.randint(10, 20)):
-            self.MakeWall()
-            self.ShowWall(random.randint(0, 1), random.randint(0, 400), random.randint(0, 400))
 
     def Draw1(self):
 
         for i in walls:
-            pygame.draw.rect(screen, (255,255,255), (100, 100 ,100, 100))
+            z = pygame.draw.rect(screen, (255, 255, 255), (i[2], i[3], i[0], i[1]))
+            p.CheckCollision(z)
 
 
 
@@ -82,19 +90,22 @@ d = Dungeon()
 p = Player()
 Player()
 d.MakeWall()
-d.Draw1()
-#d.GenRoom()
+d.MakeWall()
+# d.GenRoom()
 
 # main game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
+    screen.fill("black")
     p.checkMove()
+    d.Draw1()
+    p.playerMove()
 
-    #screen.fill("black")
-    #d.GenRoom()
-    #screen.blit(walls, (0,10))
+
+    # d.GenRoom()
+    # screen.blit(walls, (0,10))
     screen.blit(p.icon, p.rect)
     pygame.display.flip()
     clock.tick(60)
